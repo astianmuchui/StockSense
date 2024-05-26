b9dd40273a865866d26b6fa741e709199c72e495
Sebastian <astianmuchui@gmail.com>
2024-05-26 17:15:23 +0300

    Implement API Key generation

--- core/index.php
+++ core/index.php
@@ -1,6 +1,5 @@
 <?php
 
-require "../vendor/autoload.php";
 use PixelSequel\Model\Model;
 
 ini_set('display_errors', 'On');
@@ -20,6 +19,12 @@ interface Users
     public static function Create(string $name, string $email, string $phone,string $password);
     public static function Authenticate(string $email, string $password);
     public static function Exists($email): bool;
+    public static function Products(int $user_id);
+    public static function IsAuthenticated(): bool;
+    public static function ApiKey(int $user_id);
+    public static function GenerateApiKey(int $user_id);
+    public static function HasApiKey(int $user_id);
+
 }
 
 class User implements Users
@@ -113,6 +118,138 @@ class User implements Users
             {
                 return false;
             }
+         }
+    }
+
+    public static function Products(int $user_id)
+    {
+        /**
+         * @todo fetch all products belonging to the user
+         * @body Fetch all products belonging to the user
+         * @return array of objects
+         */
+
+         if (Model::Connected())
+         {
+            $data = Model::All(
+                table: "products",
+                where: [
+                    "user_id" => $user_id
+                ]
+            );
+            return $data;
+         }
+    }
+
+    public static function IsAuthenticated(): bool
+    {
+        if (isset($_SESSION['user_id']))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static function HasApiKey(int $user_id)
+    {
+        /**
+         * @todo Check if the user has an api key
+         * @body Check if the user has an api key
+         * @return bool
+         */
+
+         if (Model::Connected())
+         {
+            $data = Model::All(
+                table: "api_keys",
+                where: [
+                    "user_id" => $user_id
+                ],
+                limit: 1
+            );
+            if (count($data) > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+         }
+    }
+
+    public static function ApiKey(int $user_id)
+    {
+        /**
+         * @todo fetch the api key of the user
+         * @body Fetch the api key of the user
+         * @return string
+         */
+
+         if (Model::Connected())
+         {
+            $data = Model::All(
+                table: "api_keys",
+                where: [
+                    "user_id" => $user_id
+                ],
+                limit: 1
+            );
+            $data = $data[0];
+            return $data->api_key;
+         }
+    }
+
+    public static function DeleteApiKeys(int $user_id)
+    {
+        /**
+         * @todo Delete all api keys of the user
+         * @body Delete all api keys of the user
+         * @return bool
+         */
+
+         if (Model::Connected())
+         {
+            return (Model::Delete(
+                table: "api_keys",
+                param_t: "user_id",
+                param_n: $user_id
+            ));
+         }
+    }
+
+    public static function GenerateApiKey(int $user_id)
+    {
+        /**
+         * @todo Generate a new api key for the user
+         * @body Generate a new api key for the user
+         * @return string
+         */
+
+         if (Model::Connected())
+         {
+            if (self::HasApiKey($user_id))
+            {
+                self::DeleteApiKeys($user_id);
+            }
+
+            $api_key = password_hash(bin2hex(random_bytes(32)), PASSWORD_BCRYPT);
+            $api_key = str_replace("/", "", $api_key);
+            $api_key = str_replace("+", "", $api_key);
+            $api_key = str_replace("$", "", $api_key);
+            $api_key = substr($api_key, 0, 20);
+
+
+            return (Model::Insert(
+                table: "api_keys",
+                data: [
+                    "user_id" => $user_id,
+                    "api_key" => $api_key
+                ]
+            ));
          }
     }
 }
