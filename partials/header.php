1313fc1bb3ff968a491d12f2c369cbebaec2cf35
Sebastian <astianmuchui@gmail.com>
2024-05-26 16:26:34 +0300

    Divide UI into components

---
+++ partials/header.php
@@ -0,0 +1,17 @@
+<header class="desktop-header">
+
+<div class="title">
+    <img src="../assets/img/m_logo.png" alt="" height="50px">
+</div>
+
+
+<nav>
+    <ul>
+        <li><a href="../">Home</a></li>
+        <li><a href="../">About</a></li>
+        <li><a href="../signup">Signup</a></li>
+        <li><a href="../login">Login</a></li>
+    </ul>
+</nav>
+
+</header>
