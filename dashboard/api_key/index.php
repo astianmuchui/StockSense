a069914e4a7d8055faba954ee28b9fe6e9571516
Sebastian <astianmuchui@gmail.com>
2024-05-26 17:15:47 +0300

    Finished API Key generation

--- dashboard/api_key/index.php
+++ dashboard/api_key/index.php
@@ -1,3 +1,34 @@
+<?php
+    ini_set('display_errors', 'On');
+    error_reporting(E_ALL);
+    require "../../vendor/autoload.php";
+    require "../../core/index.php";
+
+    if (User::IsAuthenticated())
+    {
+        if (User::HasApiKey($_SESSION['user_id']))
+        {
+            $api_key = User::ApiKey($_SESSION['user_id']);
+        }
+        else
+        {
+            User::GenerateApiKey($_SESSION['user_id']);
+            $api_key = User::ApiKey($_SESSION['user_id']);
+        }
+
+        if (isset($_POST['generate']))
+        {
+            User::GenerateApiKey($_SESSION['user_id']);
+            $api_key = User::ApiKey($_SESSION['user_id']);
+            header("Location: ./");
+        }
+    }
+    else
+    {
+        header("Location: ../login/");
+    }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,7 +36,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../assets/styles/dashboard.css">
     <script src="../../assets/js/font_awesome_main.js"></script>
-    <title>Supplier Dashboard</title>
+    <title>Stocksense Dashboard</title>
 
 </head>
 <body>
@@ -17,69 +48,18 @@
         <div class="dashboard-area">
             <a href="#" class="btn-primary btn-left"> <i class="fas fa-arrow-circle-left"></i>  Menu </a>
 
-            <div class="edit-container">
-                <h4>Edit Profile</h4>
+            <div class="edit-container" style="height: 40% !important;">
 
-                <form action="#">
+                <form form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+                <h2 class="text-primary text-bold">My API KEY</h2>
 
-                    <input type="text" placeholder="Name">
-                    <input type="text" placeholder="Email">
-                    <input type="text" placeholder="Phone Number">
-                    <input type="text" placeholder="Coordinates in lattitude">
-                    <input type="text" placeholder="Coordinates in longitude">
+                    <input type="text" disabled placeholder="<?php echo $api_key ?>">
+                    <div class="grid-2">
+                    <button class="btn-gradient"> <i class="fas fa-clipboard"></i> Copy</button>
+                    <button class="btn-gradient" name="generate"> <i class="fas"></i> Generate new</button>
 
-                    <select name="county" required>
-                        <option value="">Select County</option>
-                        <option value="mombasa">Mombasa</option>
-                        <option value="mombasa">Kwale</option>
-                        <option value="kilifi">Kilifi</option>
-                        <option value="Tana river">Tana river</option>
-                        <option value="Lamu">Lamu</option>
-                        <option value="Taita">Taita taveta</option>
-                        <option value="Garrisa">Garissa</option>
-                        <option value="Wajir">Wajir</option>
-                        <option value="Mandera">Mandera</option>
-                        <option value="Marsabit">Marsabit</option>
-                        <option value="Isiolo">Isiolo</option>
-                        <option value="Meru">Meru</option>
-                        <option value="Tharaka nithi">Tharaka nithi</option>
-                        <option value="Embu">Embu</option>
-                        <option value="kitui">Kitui</option>
-                        <option value="machakos">Machakos</option>
-                        <option value="makueni">Makueni</option>
-                        <option value="Nyandarua">Nyandarua</option>
-                        <option value="Nyeri">Nyeri</option>
-                        <option value="Kirinyaga">Kirinyaga</option>
-                        <option value="Muranga">Muranga</option>
-                        <option value="Kiambu">Kiambu</option>
-                        <option value="Turkana">Turkana</option>
-                        <option value="West pokot">West pokot</option>
-                        <option value="Samburu">Samburu</option>
-                        <option value="Trans nzoia">Trans nzoia</option>
-                        <option value="Uasin gishu">Uasin gishu</option>
-                        <option value="Elgeyo marakwet">Elgeyo marakwet</option>
-                        <option value="Nandi">Nandi</option>
-                        <option value="Baringo">Baringo</option>
-                        <option value="Laikipia">Laikipia</option>
-                        <option value="Nakuru">Nakuru</option>
-                        <option value="Narok">Narok</option>
-                        <option value="Kajiado">Kajiado</option>
-                        <option value="Kericho">Kericho</option>
-                        <option value="Bomet">Bomet</option>
-                        <option value="Kakamega">Kakamega</option>
-                        <option value="Vihiga">Vihiga</option>
-                        <option value="Bungoma">Bungoma</option>
-                        <option value="Busia">Busia</option>
-                        <option value="Siaya">Siaya</option>
-                        <option value="kisumu">Kisumu</option>
-                        <option value="Homabay">Homabay</option>
-                        <option value="Migori">Migori</option>
-                        <option value="Kisii">Kisii</option>
-                        <option value="Nyamira">Nyamira</option>
-                        <option value="Nairobi">Nairobi</option>
-                    </select>
+                    </div>
 
-                    <button class="btn-primary">Update Profile</button>
 
                 </form>
 
