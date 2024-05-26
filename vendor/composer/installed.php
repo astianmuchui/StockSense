25ec31e923f43ef5490b6127ec7315e541314e51
Sebastian <astianmuchui@gmail.com>
2024-05-26 16:24:57 +0300

    Added PixelSequel

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-staging',
+        'version' => 'dev-staging',
+        'reference' => '1738e58ef05c9066bc865eaeda40a977049a8613',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-staging',
+            'version' => 'dev-staging',
+            'reference' => '1738e58ef05c9066bc865eaeda40a977049a8613',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'astianmuchui/pixelsequel' => array(
+            'pretty_version' => 'dev-staging',
+            'version' => 'dev-staging',
+            'reference' => '2744098854e973f92cee7167f94deb36795473a2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../astianmuchui/pixelsequel',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
