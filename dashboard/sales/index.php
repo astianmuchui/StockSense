8fabb50ab9b22c1c7d13d4beb9c9d6a0ac14b43a
Sebastian <astianmuchui@gmail.com>
2024-05-27 09:43:42 +0300

    Implement CRUD functionality for orders and sales

--- dashboard/sales/index.php
+++ dashboard/sales/index.php
@@ -1,3 +1,39 @@
+<?php
+    require "../../vendor/autoload.php";
+    require "../../core/index.php";
+
+    $error = "";
+
+    if (User::IsAuthenticated())
+    {
+        $products = User::Products($_SESSION['user_id']);
+        $error = "";
+        $sales = User::Sales($_SESSION['user_id']);
+        if(isset($_POST['add_sale']))
+        {
+            if (Sales::Create (
+                $_SESSION['user_id'],
+                Products::GetId($_POST['product']),
+                $_POST['qty'],
+                $_POST['customer'],
+                $_POST['payment_method'],
+                $_POST['payment_code'],
+                $_POST['date']
+            ))
+            {
+                header("Location: ./");
+            }
+            else
+            {
+                $error = "An error occurred";
+            }
+        }
+    }
+    else
+    {
+        header("Location: ../../login");
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,61 +41,71 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../assets/styles/dashboard.css">
     <script src="../../assets/js/font_awesome_main.js"></script>
-    <title>Supplier Dashboard</title>
+    <title>Stocksense Dashboard</title>
 
 </head>
 <body>
+
+
+
+    <div class="modal flex-column" id="modal">
+        <div class="modal-content flex-column">
+            <span id="closer" >&times;</span>
+
+            <form action="<?php echo $_SERVER['PHP_SELF'] ?>" class="" method="POST" enctype="multipart/form-data">
+                <h3 class="text-primary text-bold">Record New Sale </h3>
+                <small class="text-red"> <?php echo $error ?> </small>
+                <input type="text" list="options" id="myInput" placeholder="Product Name" name="product">
+                <datalist id="options">
+                <?php foreach ($products as $product): ?>
+                        <option value="<?php echo $product->name ?>"> <?php echo $product->name ?> </option>
+                    <?php endforeach ?>
+                </datalist>
+                <input type="number" placeholder="Quantity" name="qty" required>
+
+                <input type="text" placeholder="Customer Name" name="customer" required> 
+                <input type="text" placeholder="Payment Method" name="payment_method" required> 
+
+                <input type="text" placeholder="Payment Code" name="payment_code" required> 
+                <label for="">Date</label>
+                <input type="date" name="date" id="">
+
+                <button type="submit" class="btn-gradient" name="add_sale">Add Sale</button>
+            </form>
+            </div>
+    </div>
 
     <main>
         <?php include('../partials/dashboard.php') ?>
 
         <div class="dashboard-area">
+        <div class="micro-banner noshadow">
+                <h4>My Sales </h4>
 
+                <a href="#" class="btn-gradient" id="add-sale" style="padding: 10px 20px;"> <i class="fas fa-plus text-light" style="margin-right: 5px; margin-top: 4px; font-size: 13px;"></i> Add </a>
+
+            </div>
             <div class="products-container">
 
                 <div class="products">
 
-                    <h4>
-                        My Products
-
-                    </h4>
-
                     <div class="container">
 
+                    <?php if (count($sales) == 0): ?>
+                        <div class="product">
+                            <p>Sales you add will appear here</p>
+                        </div>
+                    <?php endif ?>
 
-                        <div class="product">
-                            <img src="../../assets/img/pexels-melvin-buezo-2529157.jpg" alt="">
-                            <p>Nike Sneakers</p>
-                            <p>Kes 200</p>
-                            <a href="#"> View <i class="fas fa-arrow-right"></i> </a>
-                        </div>
 
+                    <?php foreach ($sales as $sale): ?>
                         <div class="product">
-                            <img src="../../assets/img/pexels-melvin-buezo-2529157.jpg" alt="">
-                            <p>Nike Sneakers</p>
-                            <p>Kes 200</p>
-                            <a href="#">
-                                 View <i class="fas fa-arrow-right"></i> </a>
+                            <img src="../../assets/uploads/<?php echo Products::Get($sale->product_id)->image_path ?>" alt="">
+                            <p><?php echo Products::Get($sale->product_id)->name ?></p>
+                            <p>Kes <?php echo Products::Get($sale->product_id)->price * $sale->item_count ?></p>
+                            <a href="./view?id=<?php echo bin2hex(base64_encode($sale->id)) ?>"> View <i class="fas fa-arrow-right"></i> </a>
                         </div>
-                        <div class="product">
-                            <img src="../../assets/img/pexels-melvin-buezo-2529157.jpg" alt="">
-                            <p>Nike Sneakers</p>
-                            <p>Kes 200</p>
-                            <a href="#"> View <i class="fas fa-arrow-right"></i> </a>
-                        </div>
-                        <div class="product">
-                            <img src="../../assets/img/pexels-melvin-buezo-2529157.jpg" alt="">
-                            <p>Nike Sneakers</p>
-                            <p>Kes 200</p>
-                            <a href="#">
-                                 View <i class="fas fa-arrow-right"></i> </a>
-                        </div>
-                        <div class="product">
-                            <img src="../../assets/img/pexels-melvin-buezo-2529157.jpg" alt="">
-                            <p>Nike Sneakers</p>
-                            <p>Kes 200</p>
-                            <a href="#"> View <i class="fas fa-arrow-right"></i> </a>
-                        </div>
+                    <?php endforeach ?>
 
                     </div>
 
@@ -68,16 +114,16 @@
                 <div class="stats">
 
                     <div class="card">
-                        <i class="fas fa-plus"></i>
-                        <h3>Add a new Product</h3>
-                        <a href="#" class="btn-primary-bordered">Add Now</a>
+                        <i class="fas fa-boxes"></i>
+                        <h3>Count of Sales</h3>
+                        <h3> <?php echo count($sales) ?> </h3>
                     </div>
 
 
                     <div class="card">
-                        <i class="fas fa-book"></i>
-                        <h3>Add a new Category</h3>
-                        <a href="#" class="btn-primary-bordered">Add Now</a>
+                        <i class="fas fa-money-bill-wave"></i>
+                        <h3>Revenue</h3>
+                        <h3> KES <?php echo User::Revenue($_SESSION['user_id']) ?> </h3>
                     </div>
 
                 </div>
@@ -86,6 +132,19 @@
         </div>
 
     </main>
+    <script>
+        const modal = document.getElementById('modal');
+        const addSale = document.getElementById('add-sale');
+        const closer = document.getElementById('closer');
+        modal.style.display = 'none';
 
+        addSale.addEventListener('click', () => {
+            modal.style.display = 'block';
+        });
+
+        closer.addEventListener('click', () => {
+            modal.style.display = 'none';
+        });
+    </script>
 </body>
 </html>
--- dashboard/sales/view/index.php
+++ dashboard/sales/view/index.php
@@ -1,3 +1,55 @@
+<?php
+    require "../../../vendor/autoload.php";
+    require "../../../core/index.php";
+
+    $error = "";
+
+    if (User::IsAuthenticated())
+    {
+        $id = base64_decode(hex2bin($_GET['id']));
+
+        $sale = Sales::Get($id);
+        $product = Products::Get($sale->product_id);
+
+        if (isset($_POST['delete']))
+        {
+            if (Sales::Delete($id))
+            {
+                header("Location: ../");
+            }
+            else
+            {
+                $error = "An error occurred";
+            }
+
+        }
+
+        if (isset($_POST['edit']))
+        {
+            if (Sales::Update(
+                $id,
+                Products::GetId($_POST['product']),
+                $_POST['qty'],
+                $_POST['customer'],
+                $_POST['payment_method'],
+                $_POST['payment_code'],
+                $_POST['date']
+            ))
+            {
+                header("Location: ./?id=".$_GET['id']);
+
+            }
+            else
+            {
+                $error = "An error occurred";
+            }
+        }
+    }
+    else
+    {
+        header("Location: ../../../login");
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,10 +57,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../../assets/styles/dashboard.css">
     <script src="../../../assets/js/font_awesome_main.js"></script>
-    <title>Supplier Dashboard</title>
+    <title>Stocksense Dashboard</title>
 
 </head>
 <body>
+    <div class="modal-z flex-column" id="modalz">
+        <div class="modal-content flex-column x-start">
+            <span id="closez" class="btn-red">&times;</span>
+            <h1 class="text-red text-bold">Are you sure you want to take this action?</h1>
+            <small class="text-red"> <?php echo $error ?> </small>
+            <form action="<?php echo $_SERVER['PHP_SELF']."?id=".$_GET['id'] ?>" method="post" enctype="multipart/form-data">
+
+                <button type="submit" class="btn-red" name="delete"><i class="fas fa-trash"></i> Delete  </button>
+            </form>
+        </div>
+    </div>
+
+    <div class="modal flex-column" id="modal">
+        <div class="modal-content flex-column">
+            <span id="closer" >&times;</span>
+
+            <form action="<?php echo $_SERVER['PHP_SELF'] ?>?id=<?php echo $_GET['id'] ?>" class="" method="POST" enctype="multipart/form-data">
+                <h3 class="text-primary text-bold">Update Sale Information </h3>
+                <small class="text-red"> <?php echo $error ?> </small>
+                <input type="text" list="options" id="myInput" placeholder="Product Name" name="product" value="<?php echo $product->name ?>">
+                <datalist id="options">
+                <?php foreach ($products as $product): ?>
+                        <option value="<?php echo $product->name ?>"> <?php echo $product->name ?> </option>
+                <?php endforeach ?>
+                </datalist>
+                <input type="number" placeholder="Quantity" name="qty" required value="<?php echo $sale->item_count ?>">
+
+                <input type="text" placeholder="Customer Name" name="customer" required value="<?php echo $sale->customer_name ?>"">
+                <input type="text" placeholder="Payment Method" name="payment_method" required value="<?php echo $sale->payment_method ?>">
+
+                <input type="text" placeholder="Payment Code" name="payment_code" required value="<?php echo $sale->payment_code ?>"">
+                <label for="">Date</label>
+                <input type="date" name="date" id="" value="<?php echo $sale->date ?>">
+
+                <button type="submit" class="btn-gradient" name="edit">Update </button>
+            </form>
+            </div>
+    </div>
+
 
     <main>
         <?php include('../../partials/dashboard_nested.php') ?>
@@ -19,41 +110,35 @@
                 <h4>View Sale </h4>
 
 
-                <a href="#" class="btn-red">  Delete</a>
+                <a href="#" class="btn-gradient" id="edit-sale">  Edit</a>
+                <a href="#" class="btn-red" id="delete">  Delete</a>
 
             </div>
 
             <div class="view-container">
 
                 <div class="grid-container">
-                    <img src="../../../assets/img/pexels-castorly-stock-3682293.jpg" alt="">
+                    <img src="../../../assets/uploads/<?php echo $product->image_path ?>" alt="">
 
                     <div class="info">
-                        <small class="text-grey">Shoes</small>
-                        <h3>Generic High Heels</h3>
-                        <h5 class="text-grey">2 items</h5>
+                        <small class="text-grey"><?php echo Categories::Get($product->category)->name ?></small>
+                        <h3><?php echo $product->name ?></h3>
+                        <h5 class="text-grey"><?php echo $sale->item_count ?> items</h5>
 
-                        <h3 class="text-gold" >KES 5000</h3>
+                        <h3 class="text-primary" >KES <?php echo $sale->item_count * $product->price ?></h3>
 
-                        <h4>Reveiw</h4>
-
-                        <small class="flex-row" style="width: max-content;"> <!-- Will transfer this to the css file -->
-                                <i class="fas fa-star text-gold"></i><i class="fas fa-star text-gold"></i><i class="fas fa-star text-gold"></i><i class="fas fa-star text-gold"></i><i class="fas fa-star text-gold"></i>
-
-                        </small>
 
                 </div>
 
                     <div class="extra">
 
-                        <h4 class="text-gold">Sale Details</h4>
+                        <h4 class="text-primary">Sale Details</h4>
 
-                        <h5 class="text-grey"> <i class="fas fa-user text-primary"></i> Sebastian Muchui </h5>
-                        <h5 class="text-grey"> <i class="fas fa-phone-alt text-primary"></i> 0797061691 </h5>
-                        <h5 class="text-grey"> <i class="far fa-envelope text-primary"></i> sebastianmuchui79@gmail.com</h5>
+                        <h5 class="text-grey"> <i class="fas fa-user text-primary"></i> <?php echo $sale->customer_name ?> </h5>
+                        <h5 class="text-grey"> <i class="fas fa-money-bill-wave-alt text-primary"></i> Via <?php echo $sale->payment_method ?> </h5>
 
-                        <h5 class="text-grey"> <i class="fas fa-dollar-sign text-primary"></i> 4800 Earned</h5>
-                        <h5 class="text-grey"> <i class="fas fa-calendar text-primary"></i> 2023-04-08</h5>
+                        <h5 class="text-grey"> <i class="fas fa-handshake text-primary"></i> <?php echo $sale->payment_code ?> </h5>
+                        <h5 class="text-grey"> <i class="fas fa-calendar text-primary"></i> <?php echo $sale->date ?></h5>
 
 
                     </div>
@@ -66,5 +151,35 @@
 
     </main>
 
+    <script>
+
+        const modal =  document.getElementById("modal")
+        const modalz =  document.getElementById("modalz")
+        modal.style.display = "none"
+        modalz.style.display = "none"
+
+        const edit = document.getElementById("edit-sale")
+        const delete_category = document.getElementById("delete")
+
+        const closer = document.getElementById("closer")
+        const closez = document.getElementById("closez")
+
+        edit.addEventListener("click", () => {
+            modal.style.display = "flex"
+        })
+
+        delete_category.addEventListener("click", () => {
+            modalz.style.display = "flex"
+        })
+
+        closer.addEventListener("click", () => {
+            modal.style.display = "none"
+        })
+
+        closez.addEventListener("click", () => {
+            modalz.style.display = "none"
+        })
+
+    </script>
 </body>
 </html>
